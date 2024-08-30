a40a325874a798ec4f9a857a413dcac58671b9eb
Sepehr <sepehristaken@gmail.com>
2024-08-30 05:46:45 -0700

    update ApplicationContext

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,49 +1,72 @@
 package util;
 
-import entity.Loan;
-import entity.Student;
-import entity.University;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import lombok.Getter;
-import repository.CreditCardRepository;
-import repository.LoanRepository;
-import repository.StudentRepository;
-import repository.UniversityRepository;
-import repository.impl.CreditCardRepositoryImpl;
-import repository.impl.LoanRepositoryImpl;
-import repository.impl.StudentRepositoryImpl;
-import repository.impl.UniversityRepositoryImpl;
-import service.CreditCardService;
-import service.LoanService;
-import service.StudentService;
-import service.UniversityService;
-import service.impl.CreditCardServiceImpl;
-import service.impl.LoanServiceImpl;
-import service.impl.StudentServiceImpl;
-import service.impl.UniversityServiceImpl;
+import menu.*;
+import repository.*;
+import repository.impl.*;
+import service.*;
+import service.impl.*;
+
+import java.time.LocalDate;
 
 public class ApplicationContext {
     private static ApplicationContext instance;
     private EntityManagerFactory emf;
     private EntityManager em;
+    private LocalDate currentDate;
     @Getter
     private final StudentRepository studentRepository;
+
     @Getter
     private final LoanRepository loanRepository;
+
     @Getter
     private final UniversityRepository universityRepository;
+
     @Getter
     private final CreditCardRepository creditCardRepository;
+
+    @Getter
+    private final CouplesRepository couplesRepository;
+
+    @Getter
+    private final InstallmentRepository installmentRepository;
+
     @Getter
     private final StudentService studentService;
+
     @Getter
     private final LoanService loanService;
+
     @Getter
     private final UniversityService universityService;
+
     @Getter
     private final CreditCardService creditCardService;
+
+    @Getter
+    private final CouplesService couplesService;
+
+    @Getter
+    private final InstallmentService installmentService;
+
+    @Getter
+    private final LoginMenu loginMenu;
+
+    @Getter
+    private final StudentMenu studentMenu;
+
+    @Getter
+    private final LoanMenu loanMenu;
+
+    @Getter
+    private final MainMenu mainMenu;
+
+    @Getter
+    private final RepaymentMenu repaymentMenu;
 
     private ApplicationContext() {
         emf = Persistence.createEntityManagerFactory("default");
@@ -52,11 +75,22 @@ public class ApplicationContext {
         loanRepository = new LoanRepositoryImpl<>(em);
         universityRepository = new UniversityRepositoryImpl<>(em);
         creditCardRepository = new CreditCardRepositoryImpl<>(em);
+        couplesRepository = new CouplesRepositoryImpl<>(em);
+        installmentRepository = new InstallmentRepositoryImpl<>(em);
 
         studentService = new StudentServiceImpl(studentRepository);
         loanService = new LoanServiceImpl(loanRepository);
         universityService = new UniversityServiceImpl(universityRepository);
         creditCardService = new CreditCardServiceImpl(creditCardRepository);
+        couplesService = new CouplesServiceImpl(couplesRepository);
+        installmentService = new InstallmentServiceImpl(installmentRepository);
+
+
+        loginMenu = new LoginMenu(studentService, universityService);
+        repaymentMenu = new RepaymentMenu(loginMenu,loanService,creditCardService,installmentService);
+        loanMenu = new LoanMenu(loanService, creditCardService, studentService, couplesService,loginMenu);
+        studentMenu = new StudentMenu( creditCardService,  repaymentMenu,  loanMenu,  loginMenu,  loanService);
+        mainMenu = new MainMenu(loginMenu, studentMenu);
     }
 
     public static ApplicationContext getInstance() {
